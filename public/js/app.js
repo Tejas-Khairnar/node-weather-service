2cbf9a3979f6e4f0f0cb11a0b3d8972bd14fd590
Tejas-Khairnar <ktejas6892@gmail.com>
2019-04-26 18:17:27 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,3 @@
-console.log('I am javascript file');
-
-// fetch('https://jsonplaceholder.typicode.com/posts/1')
-//     .then((response) => {
-//         response.json()
-//             .then(data => {
-//                 console.log(data);
-//             })
-//     })
-
-// fetch('http://localhost:3000/weather?address=Boston')
-//     .then((response) => {
-//         response.json()
-//             .then(data => {
-//                 if(data.error) {
-//                     console.log(data.error);
-//                 } else {
-//                     console.log(data.forecast);
-//                     console.log(data.location);
-//                 }
-//             })
-//     })
-
 const formData = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
@@ -35,7 +12,7 @@ formData.addEventListener('submit', (event) => {
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
     
-    fetch('http://localhost:3000/weather?address=' + location)
+    fetch('/weather?address=' + location)
     .then((response) => {
         response.json()
             .then(data => {
